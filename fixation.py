79f4cbf19fa5185259c7c5c3dd47a03517ea57be
Yuya Inagaki <yuya1756@live.jp>
2020-12-27 18:43:17 +0900

    feat visualize type and calc radius

--- fixation.py
+++ fixation.py
@@ -1,60 +1,137 @@
+# coding: utf-8
 #
 # Fixation Class
 #
 import pandas as pd
 import numpy as np
+import cv2
 
 
 class Fixation:
-    def __init__(self, data):
-        self.timestamp = data[1]
-        self.participant_name = data[4]
-        self.event = data[34]
-        self.event_value = data[35]
-        self.stimules_name = data[68]
-        self.media_name = data[69]
-        self.eye_movement_type = data[76]
-        self.point_x = data[79]
-        self.point_y = data[80]
+  def __init__(self, data):
+    self.timestamp = data[1]
+    self.participant_name = data[4]
+    self.event = data[34]
+    self.event_value = data[35]
+    self.stimules_name = data[68]
+    self.media_name = data[69]
+    self.eye_movement_type = data[76]
+    self.point_x = data[79]
+    self.point_y = data[80]
 
-    def is_event(self):
-        return not pd.isnull(self.event)
+  def is_event(self):
+    return not pd.isnull(self.event)
 
-    def is_fixation(self):
-        return self.eye_movement_type == 'Fixation'
-        # ToDo Consider include not only Fixation but also Saccade event
+  def is_fixation(self):
+    return self.eye_movement_type == 'Fixation'
+    # ToDo Consider include not only Fixation but also Saccade event
 
 
 class FixationEvent:
-    def __init__(self, data):
-        self.start_time = data[1]
-        self.event_value = data[35]
-        self.stimules_name = ''
-        self.fixation_count = 0
-        self.fixation_sum_x = 0
-        self.fixation_sum_y = 0
+  def __init__(self, data):
+    self.start_time = data[1]
+    self.event_value = data[35]
+    self.stimules_name = ''
+    self.fixation_count = 0
+    self.fixation_sum_x = 0
+    self.fixation_sum_y = 0
 
-    def add_fixation(self, data):
-        self.fixation_count += 1
-        self.fixation_sum_x += data[79]
-        self.fixation_sum_y += data[80]
-        if self.stimules_name == '':
-            self.stimules_name = data[68]
+  def add_fixation(self, data):
+    self.fixation_count += 1
+    self.fixation_sum_x += data[79]
+    self.fixation_sum_y += data[80]
+    if self.stimules_name == '':
+      self.stimules_name = data[68]
 
-    def average_position(self):
-        if self.event_value == 'black':
-            if self.fixation_count == 0:
-                return ('error', 'error')
-            else:
-                return(self.fixation_sum_x / self.fixation_count, self.fixation_sum_y / self.fixation_count)
-        else:
-            if self.fixation_count == 0:
-                return ('error', 'error')
-            else:
-                return ((self.fixation_sum_x / self.fixation_count)-99, self.fixation_sum_y / self.fixation_count)
+  def average_position(self):
+    if self.event_value == 'black':
+      if self.fixation_count == 0:
+        return ('error', 'error')
+      else:
+        return(self.fixation_sum_x / self.fixation_count, self.fixation_sum_y / self.fixation_count)
+    else:
+      if self.fixation_count == 0:
+        return ('error', 'error')
+      else:
+        return ((self.fixation_sum_x / self.fixation_count)-99, self.fixation_sum_y / self.fixation_count)
 
-    def get_stimules_id(self):
-        if self.stimules_name == 'black':
-            return 'black'
-        else:
-            return self.stimules_name[:6]
+  def get_stimules_id(self):
+    if self.stimules_name == 'black':
+      return 'black'
+    else:
+      return self.stimules_name[:6]
+
+class TypeCollection:
+  pickup_color = (255, 0, 255)
+  def __init__(self, element_type: str, color: object, size: object):
+    self.element_type = element_type
+    self.color = color
+    self.canvas = np.zeros(size, dtype=np.uint8)
+    self.canvas.fill(255)
+    self.sum_coordinate_x = 0
+    self.sum_coordinate_y = 0
+    self.min_coordinate_x = size[0]
+    self.max_coordinate_x = 0
+    self.min_coordinate_y = size[1]
+    self.max_coordinate_y = 0
+    self.number_of_coordinates = 0
+    cv2.putText(self.canvas, element_type, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 1, cv2.LINE_AA)
+
+  # 座標の追加
+  def AddCenterPoint(self, coordinate_x: float, coordinate_y: float):
+    self.sum_coordinate_x += coordinate_x
+    self.sum_coordinate_y += coordinate_y
+    self.number_of_coordinates += 1
+    if self.min_coordinate_x > coordinate_x:
+      self.min_coordinate_x = coordinate_x
+    if self.max_coordinate_x < coordinate_x:
+      self.max_coordinate_x = coordinate_x
+    if self.min_coordinate_y > coordinate_y:
+      self.min_coordinate_y = coordinate_y
+    if self.max_coordinate_y < coordinate_y:
+      self.max_coordinate_y = coordinate_y
+    self.__DrawCenterPoint(coordinate_x, coordinate_y)
+    return (coordinate_x, coordinate_y)
+
+  # マーカーの描写
+  def VisualizeMarker(self):
+    self.__DrawAveragePoint()
+    print('タイプ：' + self.element_type)
+    print('中心点' + str(self.__GetAveragePoint()))
+    print('半径' + str(self.__GetDenseAreaRadius()))
+
+  # 中心点の取得
+  def __GetAveragePoint(self) -> tuple:
+    average_x = self.sum_coordinate_x/self.number_of_coordinates
+    average_y = self.sum_coordinate_y/self.number_of_coordinates
+    return (average_x, average_y)
+
+  # 密集直径の取得
+  def __GetDenseAreaRadius(self) -> tuple:
+    # mode 1:近い方を半径, 2:平均を半径
+    MODE = 2
+
+    radius_x = radisu_y = 0
+    if MODE == 1:
+      if self.max_coordinate_x - self.__GetAveragePoint()[0] > self.__GetAveragePoint()[0] - self.min_coordinate_x:
+        radius_x = self.__GetAveragePoint()[0] - self.min_coordinate_x
+      else:
+        radius_x = self.max_coordinate_x - self.__GetAveragePoint()[0]
+        
+      if self.max_coordinate_y - self.__GetAveragePoint()[1] > self.__GetAveragePoint()[1] - self.min_coordinate_y:
+        radius_y = self.__GetAveragePoint()[1] - self.min_coordinate_y
+      else:
+        radius_y = self.max_coordinate_y - self.__GetAveragePoint()[1]
+    elif MODE == 2:
+      radius_x = ((self.max_coordinate_x - self.__GetAveragePoint()[0]) + (self.__GetAveragePoint()[0] - self.min_coordinate_x)) / 2
+      radius_y = ((self.max_coordinate_y - self.__GetAveragePoint()[1]) + (self.__GetAveragePoint()[1] - self.min_coordinate_y)) / 2
+    return (radius_x, radius_y)
+
+  # 座標の描写
+  def __DrawCenterPoint(self, coordinate_x: float, coordinate_y: float):
+    cv2.drawMarker(self.canvas, (int(coordinate_x), int(coordinate_y)), self.color, markerType=cv2.MARKER_TILTED_CROSS)
+
+  # 平均座標の描写
+  def __DrawAveragePoint(self):
+    cv2.drawMarker(self.canvas, (tuple(map(int,self.__GetAveragePoint()))), TypeCollection.pickup_color, markerType=cv2.MARKER_STAR, markerSize=30)
+    cv2.ellipse(self.canvas, (tuple(map(int,self.__GetAveragePoint())), (tuple(map(lambda x: int(x*2),self.__GetDenseAreaRadius()))), 0), TypeCollection.pickup_color)
--- visualize_type.py
+++ visualize_type.py
@@ -2,10 +2,11 @@
 import pandas as pd
 import cv2
 import numpy as np
+from fixation import TypeCollection
 
 # setting
-DATA_FILE_NAME = 'result_all.xlsx'
-FOLDER_NAME = 'visual_all'
+DATA_FILE_NAME = 'result_2s-1.xlsx'
+FOLDER_NAME = 'visual_2s-1'
 
 print(cv2.__version__)
 print('Loading data file')
@@ -14,65 +15,43 @@ df = pd.read_excel('result/' + DATA_FILE_NAME)
 size = 1080, 1920, 3
 color = (0, 0, 0)
 
-# np.fillで白に埋める
-image_type1 = np.zeros(size, dtype=np.uint8)
-image_type2 = np.zeros(size, dtype=np.uint8)
-image_type3 = np.zeros(size, dtype=np.uint8)
-image_type4 = np.zeros(size, dtype=np.uint8)
-image_type5 = np.zeros(size, dtype=np.uint8)
-image_type6 = np.zeros(size, dtype=np.uint8)
-image_type7 = np.zeros(size, dtype=np.uint8)
-image_type1.fill(255)
-image_type2.fill(255)
-image_type3.fill(255)
-image_type4.fill(255)
-image_type5.fill(255)
-image_type6.fill(255)
-image_type7.fill(255)
-
-cv2.putText(image_type1, 'type1', (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 1, cv2.LINE_AA)
-cv2.putText(image_type2, 'type2', (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 1, cv2.LINE_AA)
-cv2.putText(image_type3, 'type3', (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 1, cv2.LINE_AA)
-cv2.putText(image_type4, 'type4', (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 1, cv2.LINE_AA)
-cv2.putText(image_type5, 'type5', (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 1, cv2.LINE_AA)
-cv2.putText(image_type6, 'type6', (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 1, cv2.LINE_AA)
-cv2.putText(image_type7, 'type7', (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 1, cv2.LINE_AA)
+type1 = TypeCollection('type1', color, size)
+type2 = TypeCollection('type2', color, size)
+type3 = TypeCollection('type3', color, size)
+type4 = TypeCollection('type4', color, size)
+type5 = TypeCollection('type5', color, size)
+type6 = TypeCollection('type6', color, size)
+type7 = TypeCollection('type7', color, size)
 
 for row in df.itertuples():
-    if row[17] == 1:
-        cv2.drawMarker(image_type1, (int(row[18]), int(row[19])), color,
-                       markerType=cv2.MARKER_TILTED_CROSS)
-    elif row[17] == 2:
-        cv2.drawMarker(image_type2, (int(row[18]), int(row[19])), color,
-                       markerType=cv2.MARKER_TILTED_CROSS)
-    elif row[17] == 3:
-        cv2.drawMarker(image_type3, (int(row[18]), int(row[19])), color,
-                       markerType=cv2.MARKER_TILTED_CROSS)
-    elif row[17] == 4:
-        cv2.drawMarker(image_type4, (int(row[18]), int(row[19])), color,
-                       markerType=cv2.MARKER_TILTED_CROSS)
-    elif row[17] == 5:
-        cv2.drawMarker(image_type5, (int(row[18]), int(row[19])), color,
-                       markerType=cv2.MARKER_TILTED_CROSS)
-    elif row[17] == 6:
-        cv2.drawMarker(image_type6, (int(row[18]), int(row[19])), color,
-                       markerType=cv2.MARKER_TILTED_CROSS)
-    elif row[17] == 7:
-        cv2.drawMarker(image_type7, (int(row[18]), int(row[19])), color,
-                       markerType=cv2.MARKER_TILTED_CROSS)
+  if row.type == 1:
+    type1.AddCenterPoint(row.average_x, row.average_y)
+  if row.type == 2:
+    type2.AddCenterPoint(row.average_x, row.average_y)
+  if row.type == 3:
+    type3.AddCenterPoint(row.average_x, row.average_y)
+  if row.type == 4:
+    type4.AddCenterPoint(row.average_x, row.average_y)
+  if row.type == 5:
+    type5.AddCenterPoint(row.average_x, row.average_y)
+  if row.type == 6:
+    type6.AddCenterPoint(row.average_x, row.average_y)
+  if row.type == 7:
+    type7.AddCenterPoint(row.average_x, row.average_y)
 
-cv2.imwrite('result/' + FOLDER_NAME + '/type1.png', image_type1)
-cv2.imwrite('result/' + FOLDER_NAME + '/type2.png', image_type2)
-cv2.imwrite('result/' + FOLDER_NAME + '/type3.png', image_type3)
-cv2.imwrite('result/' + FOLDER_NAME + '/type4.png', image_type4)
-cv2.imwrite('result/' + FOLDER_NAME + '/type5.png', image_type5)
-cv2.imwrite('result/' + FOLDER_NAME + '/type6.png', image_type6)
-cv2.imwrite('result/' + FOLDER_NAME + '/type7.png', image_type7)
-cv2.waitKey(0)
+type1.VisualizeMarker()
+type2.VisualizeMarker()
+type3.VisualizeMarker()
+type4.VisualizeMarker()
+type5.VisualizeMarker()
+type6.VisualizeMarker()
+type7.VisualizeMarker()
+
+cv2.imwrite('result/' + FOLDER_NAME + '/type1.png', type1.canvas)
+cv2.imwrite('result/' + FOLDER_NAME + '/type2.png', type2.canvas)
+cv2.imwrite('result/' + FOLDER_NAME + '/type3.png', type3.canvas)
+cv2.imwrite('result/' + FOLDER_NAME + '/type4.png', type4.canvas)
+cv2.imwrite('result/' + FOLDER_NAME + '/type5.png', type5.canvas)
+cv2.imwrite('result/' + FOLDER_NAME + '/type6.png', type6.canvas)
+cv2.imwrite('result/' + FOLDER_NAME + '/type7.png', type7.canvas)
+cv2.destroyAllWindows()
